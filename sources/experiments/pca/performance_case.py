9a6507b95f5c10ccd9e96bc454601b2179d8f4d2
Egawati Panjei <Ega@hnic.local>
2022-05-16 12:35:10 -0500

    compute performance by case for pca

---
+++ sources/experiments/pca/performance_case.py
@@ -0,0 +1,36 @@
+import sys
+import os
+
+module_path = os.path.abspath(os.path.join('..'))
+sys.path.insert(1, module_path)
+
+from metrics import get_performance_case
+
+if __name__ == '__main__':
+
+	case = 'Case1'
+	version = '_v3'
+	bfname = f'10K_{case}'
+	n_streams = 15
+	n_experiments = 30
+	window_size = 1000
+	non_data_attr = 2
+	
+	cwd = os.getcwd()
+	gt_folder = f"../../../../OutlierGen/exos/{case}{version}"
+	gt_folder = os.path.join(cwd, gt_folder)
+	print(f'gt_folder is {gt_folder}')
+	
+	performance_folder = f'pickles/performance/{case}{version}'
+
+	rel_path = f'pickles/{case}{version}'
+
+	df = get_performance_case(n_streams = n_streams,
+							  bfname = bfname, 
+							  gt_folder = gt_folder,
+							  rel_path = rel_path,
+							  performance_folder = performance_folder,
+							  n_experiments= n_experiments,
+							  window_size=window_size,
+							  non_data_attr=non_data_attr,
+							  vcase=case)
