7b3ea9c5e3d1331a3dd263bd7db88fa0836fa267
Egawati Panjei <Ega@hnic.local>
2022-05-16 11:27:29 -0500

    add modules to run experiments varying window size

---
+++ sources/experiments/dbpca/run_windows.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+N=15
+case="Case1"
+version="v3"
+D="../../../../OutlierGen/exos/${case}_${version}"
+bfname="10K_${case}"
+relpath="pickles/${case}_${version}/windows"
+windows=(100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500)
+nsets=16
+ex_num=1
+
+for ((i=1; i<$nsets; i++))
+do
+	python3 cases.py --ex_number $ex_num --nstreams $N --bfname $bfname --dfolder $D --relpath $relpath --wsize ${windows[$i-1]
+done
