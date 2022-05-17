ae84dbe610e2a82044d79ce205d3226debbe1ff7
Egawati Panjei <Ega@hnic.local>
2022-05-17 11:42:33 -0500

    case 1

--- sources/experiments/dbpca/run_case.sh
+++ sources/experiments/dbpca/run_case.sh
@@ -14,10 +14,10 @@ case="Case4"
 D="../../../../OutlierGen/exos/small_cases/${case}"
 bfname="1K_${case}"
 relpath="pickles/small_cases/${case}"
-nsets=31
+nsets=30
 wsize=100
 
-for ((i=30; i<$nsets; i++))
+for ((i=1; i<$nsets+1; i++))
 do
 	python3 cases.py --ex_number $i --nstreams $M --bfname $bfname --dfolder $D --relpath $relpath --wsize ${wsize}
 done
